60da332a49ec0aba66232b928c2dd726b4a0c896
Deepak Sharma <sadysnaat@gmail.com>
2016-04-16 21:39:57 +0530

    Implement average for Analyzable

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,10 @@
 module Analyzable
   # Your code goes here!
+  def average_price(products)
+    total = 0.0
+    products.each do |product|
+      total += product.price.to_f
+    end
+    return (total / products.size).round(2)
+  end
 end
